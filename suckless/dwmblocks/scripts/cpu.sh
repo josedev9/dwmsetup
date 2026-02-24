15bbdbcf72ffa11ab63cd7e079b89540283a80b4
Jose Rodriguez <joseangelrodrrodr@gmail.com>
2026-02-24 09:49:55 +0100

    Modified cpu usage display script

--- suckless/dwmblocks/scripts/cpu.sh
+++ suckless/dwmblocks/scripts/cpu.sh
@@ -1,8 +1,34 @@
 # top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)" %"}'
 . ~/dwmsetup/suckless/dwmblocks/scripts/themes/onedark.sh
-# echo $(^c$magenta^ top -bn 1 | awk '/^%Cpu/ { print int($2 + $4 + $6)" %"}')
-cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+#!/bin/sh
 
-printf "^c$cyan^  CPU: $cpu_val %%"
-# printf "^c$green^ "
+CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/cpu_usage"
+mkdir -p "$(dirname "$CACHE")"
 
+# Read current CPU stats
+read cpu user nice system idle iowait irq softirq steal guest < /proc/stat
+
+idle_now=$((idle + iowait))
+total_now=$((user + nice + system + idle + iowait + irq + softirq + steal))
+
+# If previous stats exist, compute usage
+if [ -f "$CACHE" ]; then
+    read prev_idle prev_total < "$CACHE"
+
+    diff_idle=$((idle_now - prev_idle))
+    diff_total=$((total_now - prev_total))
+
+    if [ "$diff_total" -gt 0 ]; then
+        usage=$((100 * (diff_total - diff_idle) / diff_total))
+    else
+        usage=0
+    fi
+else
+    usage=0
+fi
+
+# Store current stats
+echo "$idle_now $total_now" > "$CACHE"
+
+#printf "^c$cyan^CPU: %d%%\n" "$usage"
+printf "^c$cyan^ CPU: $usage %%" "$usage"
